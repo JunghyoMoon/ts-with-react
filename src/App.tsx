b50f52f4f6dabde2c5977d1e7872989baba40a72
JungHyoMoon <dsinhome70@gmail.com>
2021-06-27 22:47:18 +0900

    state with typescript = interface.. for object

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+interface IState {
+    counter: number;
+}
+
+class App extends React.Component<{}, IState> {
+    state = {
+        counter: 0,
+    };
+
+    add = () => {
+        this.setState((prev) => {
+            return {
+                counter: prev.counter + 1,
+            };
+        });
+    };
+
+    render() {
+        const { counter } = this.state;
+        return (
+            <div>
+                <span>{counter}</span>
+                <button onClick={this.add}>add 1</button>
+            </div>
+        );
+    }
 }
 
 export default App;
